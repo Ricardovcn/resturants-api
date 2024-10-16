3c1420a90869d2db6a46901c7f8f8c289a619c03
Ricardovcn <ricardovitorcn@gmail.com>
2024-10-16 07:52:04 -0300

    [Level 1] Add menus controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::API
+  
+  private 
+
+  def render_error(error_message, response_code)
+    render json: { message: error_message }, status: response_code
+  end
 end
