fbdb33b9f23c97f404e8009ff6e8d9bfbcb8d682
Ricardovcn <ricardovitorcn@gmail.com>
2024-10-16 19:03:58 -0300

    [Level 2] Changes menu_item to not accept duplicate names

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -3,6 +3,7 @@ class MenuItem < ApplicationRecord
   has_many :menus, through: :menu_items_menus
 
   validates :price_in_cents, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
+  validates :name, uniqueness: true
 
   def price_in_dolar
     self.price_in_cents / 100.0
