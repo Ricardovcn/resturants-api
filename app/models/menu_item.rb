65e5dca4763a48b2d2f400a4bb1e7714ea35e235
Ricardovcn <ricardovitorcn@gmail.com>
2024-10-16 08:32:18 -0300

    [Level 1] Changes price validation to allow nil

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -1,7 +1,7 @@
 class MenuItem < ApplicationRecord
   belongs_to :menu, class_name: "Menu", foreign_key: "menu_id"
   
-  validates :price_in_cents, numericality: { only_integer: true, greater_than: 0 }
+  validates :price_in_cents, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
 
   def price_in_dolar
     self.price_in_cents / 100.0
