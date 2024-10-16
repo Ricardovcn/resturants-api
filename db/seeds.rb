d4c8562554b697a1701d306543d09f329e5006f2
Ricardovcn <ricardovitorcn@gmail.com>
2024-10-16 08:45:21 -0300

    [Level 1] Add seeds for menu items

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,37 @@
 puts "Seeding Menus..."
 
 names = [
-  { name: "Menu Name 1" },
-  { name: "Menu Name 2" },
+  { name: "Drinks Menu" },
+  { name: "Fast Food Menu" },
 ]
 
 names.each do |menu_name|
   Menu.find_or_create_by(menu_name)
   puts "Created menu: #{menu_name}"
+end
+
+puts "Seeding Menus items..."
+
+drink_names = [
+  { name: "Water", menu_id: 1 },
+  { name: "Soda", menu_id: 1 },
+  { name: "Mojito", menu_id: 1 }
+]
+
+fast_food_names = [
+  { name: "Fries", menu_id: 2 },
+  { name: "Hamburguer", menu_id: 2 },
+  { name: "Pizza", menu_id: 2 }
+]
+
+drink_names.each do |drink|
+  MenuItem.find_or_create_by(drink)
+  puts "Created menu item: #{drink}"
+end
+
+fast_food_names.each do |fast_food|
+  MenuItem.find_or_create_by(fast_food)
+  puts "Created menu item: #{fast_food}"
 end
 
 puts "Seeding completed!"
