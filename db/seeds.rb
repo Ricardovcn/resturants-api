5a590c9a52b5dd334cea7624ad5b667a334889ac
Ricardovcn <ricardovitorcn@gmail.com>
2024-10-19 10:32:45 -0300

    [Level 3] Updates seed and schema

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,23 +10,23 @@ fast_food_menu = Menu.find_or_create_by({ name: "Fast Food Menu", restaurant_id:
   
 puts "Seeding Menus items..."
 
-water = MenuItem.find_or_create_by({ name: "Water"})
-soda = MenuItem.find_or_create_by({ name: "Soda"})
-mojito = MenuItem.find_or_create_by({ name: "Mojito"})
+water = MenuItem.find_or_create_by({ restaurant_id: drinks_menu.restaurant_id, name: "Water"})
+soda = MenuItem.find_or_create_by({ restaurant_id: drinks_menu.restaurant_id, name: "Soda"})
+mojito = MenuItem.find_or_create_by({ restaurant_id: drinks_menu.restaurant_id, name: "Mojito"})
 
-fries = MenuItem.find_or_create_by({ name: "Fries"})
-hamburguer = MenuItem.find_or_create_by({ name: "Hamburguer"})
-pizza = MenuItem.find_or_create_by({ name: "Pizza"})
+fries = MenuItem.find_or_create_by({ restaurant_id: fast_food_menu.restaurant_id, name: "Fries"})
+hamburguer = MenuItem.find_or_create_by({ restaurant_id: fast_food_menu.restaurant_id, name: "Hamburguer"})
+pizza = MenuItem.find_or_create_by({ restaurant_id: fast_food_menu.restaurant_id, name: "Pizza"})
 
 puts "Seedings Associations"
 
-MenuItemMenu.find_or_create_by({menu_id: drinks_menu.id, menu_item_id: water.id})
-MenuItemMenu.find_or_create_by({menu_id: drinks_menu.id, menu_item_id: soda.id})
-MenuItemMenu.find_or_create_by({menu_id: drinks_menu.id, menu_item_id: mojito.id})
+MenuItemMenu.find_or_create_by({ menu_id: drinks_menu.id, menu_item_id: water.id })
+MenuItemMenu.find_or_create_by({ menu_id: drinks_menu.id, menu_item_id: soda.id })
+MenuItemMenu.find_or_create_by({menu_id: drinks_menu.id, menu_item_id: mojito.id })
 
-MenuItemMenu.find_or_create_by({menu_id: fast_food_menu.id, menu_item_id: fries.id})
-MenuItemMenu.find_or_create_by({menu_id: fast_food_menu.id, menu_item_id: hamburguer.id})
-MenuItemMenu.find_or_create_by({menu_id: fast_food_menu.id, menu_item_id: pizza.id})
+MenuItemMenu.find_or_create_by({ menu_id: fast_food_menu.id, menu_item_id: fries.id })
+MenuItemMenu.find_or_create_by({ menu_id: fast_food_menu.id, menu_item_id: hamburguer.id })
+MenuItemMenu.find_or_create_by({ menu_id: fast_food_menu.id, menu_item_id: pizza.id })
 
 
 puts "Seeding completed!"
