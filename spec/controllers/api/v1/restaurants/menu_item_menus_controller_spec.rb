9ddcc533365174bddb1fcb7a047e8f6060bed509
Ricardovcn <ricardovitorcn@gmail.com>
2024-10-20 10:17:47 -0300

    [Level 3] Update controller tests

--- spec/controllers/api/v1/restaurants/menu_item_menus_controller_spec.rb
+++ spec/controllers/api/v1/restaurants/menu_item_menus_controller_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe Api::V1::Restaurants::MenuItemMenusController, type: :controller 
   describe "POST /create" do
     context "gets valid parameters" do
       it 'returns a 200 code and the created menu_item_menus' do
-        post :create, params: { restaurant_id: 2, menu_id: 1, menu_item_id: 6 }
-
+        post :create, params: { restaurant_id: 2, menu_id: 1, menu_item_id: 4 } 
         expect(response).to have_http_status :ok
         json_response = JSON.parse(response.body)
         expect(json_response["menu_id"]).to be_present
@@ -18,8 +17,7 @@ RSpec.describe Api::V1::Restaurants::MenuItemMenusController, type: :controller 
     context "gets valid parameters" do
       it 'returns a 204 code' do
         table_size_after_delete = MenuItemMenu.all.size - 1
-        delete :destroy, params: { restaurant_id: 2, menu_id: 1, menu_item_id: 1}   
-
+        delete :destroy, params: { restaurant_id: 2, menu_id: 1, menu_item_id: 1}           
         expect(response).to have_http_status :no_content
         expect(MenuItemMenu.all.size).to eql(table_size_after_delete)
       end
